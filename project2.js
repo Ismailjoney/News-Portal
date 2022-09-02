d24b297e0fee6e8f072b5cf68e39618fe57c1f21
Ismailjoney <mohammadismailjoney@gmail.com>
2022-09-02 21:38:28 +0600

    make anavation bar dinamycally

--- project2.js
+++ project2.js
@@ -1,1 +1,24 @@
-console.log(`jgkiu`)
+const loadNews =() =>{
+    fetch(`https://openapi.programming-hero.com/api/news/categories`)
+    .then(res => res.json())
+    .then(data => allNewsCatagory(data.data.news_category))
+}
+
+const allNewsCatagory = (news) => {
+    // console.log(news);
+    const allNewsNav = document.getElementById(`allNewsNav`);
+    news.forEach(allNews => {
+        console.log(allNews.category_name)
+         const newDiv = document.createElement(`div`);
+         newDiv.innerHTML = `
+         <ul class="nav d-inline-block">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">${allNews.category_name}</a>
+            </li>
+       </ul>
+         `;
+         allNewsNav.appendChild(newDiv);
+    });
+}
+
+loadNews();
