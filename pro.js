cfa398eecfda5fa9b275c2cc1c887dae9fc2b00c
Ismailjoney <mohammadismailjoney@gmail.com>
2022-09-02 21:53:55 +0600

    onclick func .. add

--- pro.js
+++ pro.js
@@ -1,1 +1,29 @@
 //https://github.com/Programming-Hero-Web-Course4/b6-news-portal-assignment-Ismailjoney
+
+
+const loadNews =() =>{
+    fetch(`https://openapi.programming-hero.com/api/news/categories`)
+    .then(res => res.json())
+    .then(data => allNewsCatagory(data.data.news_category))
+}
+
+const allNewsCatagory = (news) => {
+    // console.log(news);
+    const allNewsNav = document.getElementById(`allNewsNav`);
+    news.forEach(allNews => {
+        console.log(allNews.category_name)
+         const newDiv = document.createElement(`div`);
+         newDiv.innerHTML = `
+         <ul class="nav d-inline-block">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">${allNews.category_name}</a>
+            </li>
+       </ul>
+         `;
+         allNewsNav.appendChild(newDiv);
+    });
+}
+
+
+
+loadNews();
--- project2.js
+++ project2.js
@@ -8,17 +8,28 @@ const allNewsCatagory = (news) => {
     // console.log(news);
     const allNewsNav = document.getElementById(`allNewsNav`);
     news.forEach(allNews => {
-        console.log(allNews.category_name)
+        // console.log(allNews)
          const newDiv = document.createElement(`div`);
          newDiv.innerHTML = `
          <ul class="nav d-inline-block">
             <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">${allNews.category_name}</a>
+            <a class="nav-link active" aria-current="page" href="#" onclick ="allnewBlog('${allNews.category_id}')">${allNews.category_name}</a>
             </li>
        </ul>
          `;
          allNewsNav.appendChild(newDiv);
     });
 }
+
+//
+const allnewBlog = (id) => {
+    console.log(id);
+    fetch(`https://openapi.programming-hero.com/api/news/category/01`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
+
+
+ 
 
 loadNews();
