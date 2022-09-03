3f5df1c63efc64279f4ac178d480c312e5685ef9
Ismailjoney <mohammadismailjoney@gmail.com>
2022-09-04 01:15:59 +0600

    three question

--- project2.js
+++ project2.js
@@ -11,16 +11,20 @@ const allNewsCatagory = (news) => {
         // console.log(allNews)
          const newDiv = document.createElement(`div`);
          newDiv.innerHTML = `
-         <ul class="nav d-inline-block">
-            <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#" onclick ="allnewBlog('${allNews.category_id}')">${allNews.category_name}</a>
+         <ul class="nav   ">
+            <li class="nav-item ">
+            <a class="nav-link active loadSpin" aria-current="page" href="#" onclick ="allnewBlog('${allNews.category_id}')">${allNews.category_name}</a>
             </li>
        </ul>
          `;
          allNewsNav.appendChild(newDiv);
     });
+
+
 }
 
+
+ 
 //onclick function  call :
 const allnewBlog = (id) => {
     fetch(`https://openapi.programming-hero.com/api/news/category/${id}`)
@@ -34,7 +38,7 @@ const displayNews = (news) => {
     const displayNewsBlog = document.getElementById(`displayNewsBlog`);
     displayNewsBlog.innerHTML="";
 
-      news.forEach(singleNews =>{
+      news.forEach(singleNews => {
         console.log(singleNews);
         const div = document.createElement(`div`);
         div.innerHTML = `
@@ -51,10 +55,10 @@ const displayNews = (news) => {
                         <div class="d-flex justify-content-between align-items-center mt-4">
                             <div class="d-flex">
                                 <img class="news-writer" src="${singleNews.author.img}" class="img-fluid rounded-start" alt="...">
-                                <p class="card-text p-3 fw-bold">${singleNews.author.name ? singleNews.author.name : `no found writer`}</p> 
+                                <p class="card-text p-3 fw-bold">${singleNews.author.name ? singleNews.author.name : `no data avilable`}</p> 
                             </div>
                             <div>
-                                <p>view : ${singleNews.total_view ? singleNews.total_view : "no view"} </p>
+                                <p class ="fw-bold">view : ${singleNews.total_view ? singleNews.total_view : "no view"} </p>
                             </div>
                             <div>
                                 <button type="button" class="btn btn-success" onclick ="newsDetails('${singleNews._id}')" data-bs-toggle="modal" data-bs-target="#exampleModal">News info</button>
@@ -80,15 +84,21 @@ const displayNews = (news) => {
     console.log(authorInfo.author.name)
      const modalNewsInfo = document.getElementById(`modalNewsInfo`);
      modalNewsInfo.innerHTML = `
-        <p>Name : ${authorInfo.author.name ? authorInfo.author.name : " no found" }</p>
-        <p>Ratting :Badge : ${authorInfo.rating.badge},  <br><br> Number :  ${authorInfo.rating.number} </p>
-        <p>view : ${authorInfo.total_view ? authorInfo.total_view : "no view"} </p>
-
-
+     <div class ="d-flex mt-3 mb-3 align-items-center">
+        <img class="news-writer" src="${authorInfo.thumbnail_url
+        }" class="img-fluid rounded-start" alt="...">
+        <p class="fw-bold ms-3"> ${authorInfo.author.name ? authorInfo.author.name : " no found" }</p>
+     </div>
+        <p class="fw-bold">Badge : ${authorInfo.rating.badge},  <br><br> Number :  ${authorInfo.rating.number} </p>
+        <p class="fw-bold">view : ${authorInfo.total_view ? authorInfo.total_view : "no view"} </p>
         `;
       
   }
+ 
   
+ 
+
+
 
  
 
